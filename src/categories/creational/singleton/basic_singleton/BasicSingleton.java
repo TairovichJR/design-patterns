84b8b32ca2c6e56795bf155217da85a77c4f1294
TairovichJR <yokubov.omonjon@gmail.com>
2020-12-20 23:34:39 -0500

    basic singleton

---
+++ src/categories/creational/singleton/basic_singleton/BasicSingleton.java
@@ -0,0 +1,35 @@
+package categories.creational.singleton.basic_singleton;
+
+public class BasicSingleton {
+
+    private BasicSingleton(){
+
+    }
+
+    private static final BasicSingleton INSTANCE = new BasicSingleton();
+
+    public static BasicSingleton getInstance(){
+        return INSTANCE;
+    }
+
+    private int value = 0;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
+
+class Demo{
+    public static void main(String[] args) {
+
+        BasicSingleton singleton = BasicSingleton.getInstance();
+
+        singleton.setValue(123);
+        System.out.println(singleton.getValue());
+
+    }
+}
